acd2f6ecfe689db592b3c0df56d1bb77c550f333
Pravallika <pravali9603@gmail.com>
2024-10-15 15:03:35 +0530

    check

--- src/main/java/org/neoteric/java/Test.java
+++ src/main/java/org/neoteric/java/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("ngrok test");
         System.out.println("ngrok test11");
-
         System.out.println("ngrok test568789");
+        System.out.println("setup done");
     }
 }
