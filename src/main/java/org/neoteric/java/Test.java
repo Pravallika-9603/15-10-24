39d22e5c2ab7087b7e0f4c9b78d95fd568f19c76
Pravallika <pravali9603@gmail.com>
2024-10-15 14:55:55 +0530

    commit

--- src/main/java/org/neoteric/java/Test.java
+++ src/main/java/org/neoteric/java/Test.java
@@ -3,5 +3,6 @@ package org.neoteric.java;
 public class Test {
     public static void main(String[] args) {
         System.out.println("ngrok test");
+        System.out.println("ngrok test11");
     }
 }
